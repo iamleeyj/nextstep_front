8a9f74e7d8e40ed87b6edd302e443ef08b7717d8
imleeyj <your-email@example.com>
2025-04-22 14:02:28 +0900

    update

--- nsfront/src/screens/LoginPage/LoginPage.tsx
+++ nsfront/src/screens/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "../../components/ui/button";
-import { Card, CardContent } from "../../components/ui/card";
+import { Button } from "../../components/ui/button-1";
+import { Card, CardContent } from "../../components/ui/card-1";
 import { Input } from "../../components/ui/input";
 
 export const LoginPage = (): JSX.Element => {
